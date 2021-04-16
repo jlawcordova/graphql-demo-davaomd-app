3f0dedeb5e1692a5f74e8d7e1f3bdd9f9434edb4
J. Law. Cordova <jlaw.cordova@gmail.com>
2021-04-16 15:13:28 +0800

    Add navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'doctor-profile',
+    component: DoctorProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorProfileComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
